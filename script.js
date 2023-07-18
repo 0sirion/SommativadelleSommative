cb43924cebdd576d4770bda3f9ff4a9623401612
0sirion <salvatore.arcuri.93@gmail.com>
2023-07-18 17:46:14 +0200

    first commit

--- script.js
+++ script.js
@@ -35,11 +35,45 @@ const smartphones = [
     memoriaStorage: 256,
     immagine: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7PZMC1KerFBykOM8U70eVEawa1yNiM9KZh7PHu74Mu_Z4nZleB2I-P5DCivSw3ImEpu4&usqp=CAU'
   },
-  
+  {
+    id: 5,
+    prezzo: 1100,
+    marca: 'Apple',
+    modello: 'iPhone 13',
+    memoriaRAM: 6,
+    memoriaStorage: 256,
+    immagine: 'https://cdn.moglix.com/p/VyNL75lcCIoSt.jpg'
+  },
+  {
+    id: 6,
+    prezzo: 900,
+    marca: 'Samsung',
+    modello: 'Galaxy S20 FE',
+    memoriaRAM: 8,
+    memoriaStorage: 128,
+    immagine: 'https://d33i50qtfold6x.cloudfront.net/cate/new_product/Reno8+Lite_Black_2400x2400_Cover.webp'
+  },
+  {
+    id: 7,
+    prezzo: 1300,
+    marca: 'Google',
+    modello: 'Pixel 6 Pro',
+    memoriaRAM: 8,
+    memoriaStorage: 512,
+    immagine: 'https://data.wikomobile.com/documents/images/IT/dbd2f986d94529fc20c0864e2c5c978c.jpg'
+  },
+  {
+    id: 8,
+    prezzo: 950,
+    marca: 'OnePlus',
+    modello: 'Nord 2',
+    memoriaRAM: 6,
+    memoriaStorage: 128,
+    immagine: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7PZMC1KerFBykOM8U70eVEawa1yNiM9KZh7PHu74Mu_Z4nZleB2I-P5DCivSw3ImEpu4&usqp=CAU'
+  }
 ];
 
-
-const computerDesktops = [
+const computerDesktop = [
   {
     id: 0,
     prezzo: 1000,
@@ -83,7 +117,7 @@ const computerDesktops = [
     marcaSchedaMadre: 'Gigabyte',
     quantitaRAM: 16,
     quantitaStorage: 512,
-    immagine: 'https://st.depositphotos.com/1000128/2642/i/600/depositphotos_26429241-stock-photo-desktop-computer-with-touchscreen-interface.jpg'
+    immagine: 'https://www.pngplay.com/wp-content/uploads/2/Desktop-PC-Transparent-Images.png'
   },
   {
     id: 5,
@@ -110,78 +144,133 @@ const computerDesktops = [
     marcaSchedaMadre: 'ASRock',
     quantitaRAM: 8,
     quantitaStorage: 2,
-    immagine: 'https://shorturl.at/uxMR7'
+    immagine: 'https://w7.pngwing.com/pngs/524/847/png-transparent-dell-vostro-laptop-desktop-computers-computer-desktop-pc-computer-network-electronics-computer.png'
   }
 ];
 
 
+const cartArray = [
+];
 
-function loadScriptSmartphone() {
-  const smartphoneContainer = document.querySelector('.smartphone-container');
+function desktopLoader() {
+  const desktopContainer = document.querySelector(".desktopContainer");
+  
+  computerDesktop.map(desktop => {
+    const desktopCard = document.createElement('div');
+    desktopCard.classList.add('desktopCard');
 
-  smartphones.forEach(smartphone => {
-    const card = document.createElement('div');
-    card.classList.add('smartphone-card');
+    const brand = document.createElement('h1')
+    brand.textContent = desktop.marcaProcessore
+    desktopCard.appendChild(brand)
 
     const image = document.createElement('img');
-    image.src = smartphone.immagine;
-    card.appendChild(image);
-
-    const brand = document.createElement('h3');
-    brand.textContent = smartphone.marca;
-    card.appendChild(brand);
-
-    const model = document.createElement('p');
-    model.textContent = smartphone.modello;
-    card.appendChild(model);
+    image.classList.add("desktopImg")
+    image.src = desktop.immagine
+    desktopCard.appendChild(image)
 
     const price = document.createElement('p');
-    price.textContent = `Price: $${smartphone.prezzo}`;
-    card.appendChild(price);
+    price.textContent = `Prezzo: ${desktop.prezzo}€`
+    price.classList.add('desktopPrice')
+    desktopCard.appendChild(price)
 
-    const addToCartButton = document.createElement('button');
-    addToCartButton.textContent = 'Aggiungi al carrello';
-    addToCartButton.addEventListener('click', () => {
-      console.log(smartphone);
+
+
+
+
+    const detailsLink = document.createElement('a');
+    detailsLink.href = "./details.html";
+    deta
+    detailsLink.textContent = "vedi dettagli";
+    detailsLink.addEventListener('click', () => {
+      localStorage.setItem('selectedDesktop', JSON.stringify(desktop));
     });
-    card.appendChild(addToCartButton);
+
+    desktopCard.appendChild(detailsLink);
+    // const detailsLink = document.createElement('a');
+    // detailsLink.href = "./Details.html"
+    // detailsLink.textContent = "vedi dettagli"
+    // detailsLink.addEventListener('click', () => {
+    //   globalize(desktop);
+    // });
+
+      
+      
+      
+    
+
+    // detailsButton.textContent = 'Dettagli';
+    // detailsButton.classList.add('detailsButton');
+    // detailsButton.addEventListener('click', () => {
+    //   console.log(desktop);
+    // });
+
+    
 
 
-    smartphoneContainer.appendChild(card);
+    desktopContainer.appendChild(desktopCard)
   });
+
+}
+
+function globalize(selectedDesktop) {
+  window.desktop = selectedDesktop;
+}
+
+
+function smartphonesLoader() {
+  const link = 'https://encr.pw/xK2Pg'
+  
+
+
+}
+
+
+function detailsLoader() {
+  const selectedDesktop = JSON.parse(localStorage.getItem('selectedDesktop'));
+  console.log(selectedDesktop);
+
+  // Access the properties of the selectedDesktop object and update the HTML elements
+  document.getElementById('detailsImage').src = selectedDesktop.immagine;
+  document.getElementById('detailsTitle').textContent = selectedDesktop.marcaProcessore;
+  document.getElementById('detailsPrice').textContent = `Prezzo: ${selectedDesktop.prezzo}€`;
+  document.getElementById('detailsMarcaProcessore').textContent = `Marca Processore: ${selectedDesktop.marcaProcessore}`;
+  document.getElementById('detailsMarcaSchedaMadre').textContent = `Marca Scheda Madre: ${selectedDesktop.marcaSchedaMadre}`;
+  document.getElementById('detailsQuantitaRAM').textContent = `Quantità RAM: ${selectedDesktop.quantitaRAM}`;
+  document.getElementById('detailsQuantitaStorage').textContent = `Quantità Storage: ${selectedDesktop.quantitaStorage}`;
+
+  // Clear the selectedDesktop from localStorage after retrieving the data
+  localStorage.removeItem('selectedDesktop');
 }
 
 
 
-function loadScriptDesktop() {
-  const desktopContainer = document.querySelector('.desktop-container');
 
-  computerDesktops.forEach(desktop => {
-    const card = document.createElement('div');
-    card.classList.add('desktop-card');
 
-    const image = document.createElement('img');
-    image.src = desktop.immagine;
-    card.appendChild(image);
 
-    const marcaProcessore = document.createElement('h3');
-    marcaProcessore.textContent = desktop.marcaProcessore;
-    card.appendChild(marcaProcessore);
 
-    const price = document.createElement('p');
-    price.textContent = `Price: $${desktop.prezzo}`;
-    card.appendChild(price);
 
-    const addToCartButton = document.createElement('button');
-    addToCartButton.textContent = 'Aggiungi al carrello';
-    addToCartButton.addEventListener('click', () => {
-      console.log(desktop);
-    });
-    card.appendChild(addToCartButton);
 
-    desktopContainer.appendChild(card);
-  });
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
